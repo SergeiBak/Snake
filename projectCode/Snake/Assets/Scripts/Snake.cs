84405b44792e12914d2e8864a25965cbff2d16c7
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2022-05-31 17:54:06 -0700

    Snake Growing

--- projectCode/Snake/Assets/Scripts/Snake.cs
+++ projectCode/Snake/Assets/Scripts/Snake.cs
@@ -6,10 +6,20 @@ public class Snake : MonoBehaviour
 {
     private Vector2 direction = Vector2.right;
 
+    private List<Transform> segments;
+    [SerializeField]
+    private Transform segmentPrefab;
+
     public float speed = 20f;
     public float speedMultiplier = 1f;
 
     private float nextUpdate;
+
+    private void Start()
+    {
+        segments = new List<Transform>();
+        segments.Add(this.transform);
+    }
 
     private void Update()
     {
@@ -38,8 +48,29 @@ public class Snake : MonoBehaviour
             return;
         }
 
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
         transform.position = new Vector3(Mathf.Round(transform.position.x) + direction.x, Mathf.Round(transform.position.y) + direction.y, 0f);
 
         nextUpdate = Time.time + (1f / (speed * speedMultiplier));
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = segments[segments.Count - 1].position;
+
+        segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
     }
 }
